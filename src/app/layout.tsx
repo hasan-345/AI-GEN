cd14e74f997bd5b9746313c35014ae281a27b053
hasan-345 <ahtishamhassan345@gmail.com>
2024-08-29 00:21:19 -0700

    chnages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from '@vercel/analytics/react';
 
 const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"], weight: ["400","500","600","700"], variable: "--font-ibm-plex" });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>{children}</body>
     </html>
+    <Analytics/>
     </ClerkProvider>
   );
 }
